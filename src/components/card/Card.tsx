eec895d2010a1d65daaf64b426dc66ed6f67e135
BulochkaBu <nastyaerm@mail.ru>
2024-01-07 20:36:19 +0200

    updated styles Card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -9,9 +9,9 @@ interface CardProps {
 export default function Card({ id, name, description }: CardProps) {
   return (
     <Link to={`/breeds/${id}`}>
-      <div className="group flex flex-col items-center w-full h-56 md:w-80 border border-orange-500 rounded-xl shadow-md bg-orange-50 p-3 md:p-5 relative md:hover:scale-95 ">
+      <div className="group flex flex-col items-center w-full h-56 md:w-80 border border-orange-500 rounded-xl shadow-md bg-orange-50 p-3 md:p-5 relative md:hover:scale-95">
         <h2 className="text-orange-600 text-center text-lg">{name}</h2>
-        <p className="text-black text-md overflow-y-auto text-left mt-2">{description}</p>
+        <p className="text-black text-md text-left mt-2 line-clamp-5">{description}</p>
       </div>
     </Link>
   );
